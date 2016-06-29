71bac62da2e247049d4ba401da6d509dc62b960f
tabuna <bliz48rus@gmail.com>
2016-06-30 01:58:48 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+
+
+    mix.less('./resources/assets/less/app.less', './public/dist/css/orchid.css');
+    mix.copy('./resources/assets/vendor/bootstrap/dist/fonts/', './public/dist/fonts');
+    mix.copy('./resources/assets/vendor/font-awesome/fonts/', './public/dist/fonts');
+    mix.copy('./resources/assets/vendor/summernote/dist/fonts/', './public/dist/fonts');
+    mix.copy('./resources/assets/vendor/simple-line-icons/fonts/','./public/dist/fonts');
+    
+    mix.scripts([
+        "./resources/assets/vendor/jquery/dist/jquery.min.js",
+        "./resources/assets/vendor/bootstrap/dist/js/bootstrap.min.js",
+        "./resources/assets/vendor/vue/dist/vue.js",
+        "./resources/assets/vendor/vue-resource/dist/vue-resource.js",
+        "./resources/assets/js/app.js",
+        "./resources/assets/js/modules/**",
+        "./resources/assets/js/components/**",
+        "./resources/assets/js/directives/**",
+        "./resources/assets/vendor/summernote/dist/summernote.min.js"
+    ], './public/dist/js/orchid.js');
+
+
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+    <link href="/dist/css/orchid.css" rel="stylesheet" type="text/css">
+</head>
+
+
+<body>
+
+
+<!-- header -->
+<header id="header" class="navbar bg-white-only padder-v b-b box-shadow">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="btn btn-link visible-xs pull-right m-r" type="button" data-toggle="collapse"
+                    data-target=".navbar-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a href="#" class="navbar-brand m-r-lg">
+                <img src="/img/logo.png" class="m-r-sm">
+            </a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="#">О проекте</a>
+                </li>
+            </ul>
+
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">О проекте</a></li>
+                <li><a href="#">Инвестору</a></li>
+                <li><a href="#">Потребителям</a></li>
+                <li><a href="#">Контакты</a></li>
+
+                <li>
+
+
+                    <form class="navbar-form navbar-form-sm navbar-left w-sm" role="search">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input type="text" class="form-control input-sm bg-light no-border rounded padder"
+                                       placeholder="Поиск...">
+              <span class="input-group-btn">
+                <button type="submit" class="btn btn-sm bg-light rounded"><i class="fa fa-search"></i></button>
+              </span>
+                            </div>
+                        </div>
+                    </form>
+
+                </li>
+
+
+            </ul>
+
+
+        </div>
+    </div>
+</header>
+<!-- / header -->
+<div id="content" class="">
+
+
+    <div class="container">
+
+        <div id="carousel-example-generic" class="carousel slide box-shadow" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner bg-grey" role="listbox">
+                <div class="item active"><img alt="First slide [900x500]"
+                                              src="/img/slide-1.png" data-holder-rendered="true"></div>
+                <div class="item"><img alt="First slide [900x500]"
+                                       src="/img/slide-1.png" data-holder-rendered="true"></div>
+                <div class="item"><img alt="First slide [900x500]"
+                                       src="/img/slide-1.png" data-holder-rendered="true"></div>
+            </div>
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <i class="fa fa-angle-left" aria-hidden="true"></i>
+                <span class="sr-only">Previous</span>
+            </a> <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <i class="fa fa-angle-right" aria-hidden="true"></i> <span class="sr-only">Next</span>
+            </a></div>
+
+    </div>
+
+
+    <div class="container m-t-xxl">
+
+        <div class="row">
+            <div class="col-md-6">
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="panel panel-default bg-dark b-light">
+                            <div class="panel-hover">
+                                <a href="/virtualnyy-tur/" class="block wrapper-lg">
+                                    <div class="panel-hover-title b-b">
+                                        <p class="h4">Управляющая компания</p>
+                                    </div>
+                                    <div class="panel-hover-body">
+                                        Highly customizable NVD3, rickshaw
+                                    </div>
+                                    <div class="panel-hover-icon">
+                                        <i class="fa fa-user"></i>
+                                    </div>
+
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="panel panel-default bg-dark b-light">
+                            <div class="panel-hover">
+                                <a href="/virtualnyy-tur/" class="block wrapper-lg">
+                                    <div class="panel-hover-title b-b">
+                                        <p class="h4">Управляющая компания</p>
+                                    </div>
+                                    <div class="panel-hover-body">
+                                        Highly customizable NVD3, rickshaw
+                                    </div>
+                                    <div class="panel-hover-icon">
+                                        <i class="fa fa-user"></i>
+                                    </div>
+
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="panel panel-default bg-dark b-light">
+                            <div class="panel-hover">
+                                <a href="/virtualnyy-tur/" class="block wrapper-md">
+                                    <div class="panel-hover-title b-b">
+                                        <p>Управляющая компания</p>
+                                    </div>
+                                    <div class="panel-hover-body">
+                                        Highly customizable NVD3, rickshaw
+                                    </div>
+                                    <div class="panel-hover-icon">
+                                        <i class="fa fa-user"></i>
+                                    </div>
+
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="panel panel-default bg-dark b-light">
+                            <div class="panel-hover">
+                                <a href="/virtualnyy-tur/" class="block wrapper-md">
+                                    <div class="panel-hover-title b-b">
+                                        <p>Управляющая компания</p>
+                                    </div>
+                                    <div class="panel-hover-body">
+                                        Highly customizable NVD3, rickshaw
+                                    </div>
+                                    <div class="panel-hover-icon">
+                                        <i class="fa fa-user"></i>
+                                    </div>
+
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="panel panel-default bg-dark b-light">
+                            <div class="panel-hover">
+                                <a href="/virtualnyy-tur/" class="block wrapper-md">
+                                    <div class="panel-hover-title b-b">
+                                        <p>Управляющая компания</p>
+                                    </div>
+                                    <div class="panel-hover-body">
+                                        Highly customizable NVD3, rickshaw
+                                    </div>
+                                    <div class="panel-hover-icon">
+                                        <i class="fa fa-user"></i>
+                                    </div>
+
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+            <div class="col-md-6">
+
+                <div class="row">
+                    <article class="col-md-6">
+                        <p>
+                            <span class="text-center h4 font-thin m-r-lg">Новости</span>
+
+                            <a class="text-center h5  font-thin"><span class="text-danger">Все новости</span>
+                                <button
+                                        class="btn btn-danger btn-rounded btn-sm btn-icon btn-default m-l-sm">
+                                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                </button>
+                            </a>
+                        </p>
+
+                        <div class="">
+                            <img src="/img/news.jpeg" class="img-responsive">
+                            <div class="caption m-t-xs">
+                                <small class="font-thin">26 февраля 2015</small>
+                                <p class="small">Руководство компании «Кемин Индастриз» рассматривает возможность
+                                    размещения
+                                    предприятия...</p>
+                            </div>
+                        </div>
+
+
+                    </article>
+                    <article class="col-md-6">
+                        <figure>
+                            <figcaption class="text-center h4 font-thin">Целевые показатели роста</figcaption>
+                            <p><img src="/img/rost.png" class="img-responsive"></p>
+                        </figure>
+                    </article>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="container m-t-xxl">
+        <div class="col-sm-4 text-center">
+
+            <svg width="60px" height="60px" viewBox="0 0 60 60" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                <!-- Generator: Sketch 3.2.2 (9983) - http://www.bohemiancoding.com/sketch -->
+                <title>Parachute</title>
+                <desc>Created with Sketch.</desc>
+                <defs></defs>
+                <g id="outlined" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                    <g id="Activities_sliced" sketch:type="MSLayerGroup"
+                       transform="translate(-360.000000, -960.000000)"></g>
+                    <g id="Activities" sketch:type="MSLayerGroup" transform="translate(-374.000000, -961.000000)"
+                       stroke="#535353" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <g id="Parachute" transform="translate(380.000000, 965.000000)" sketch:type="MSShapeGroup">
+                            <path d="M48,24 C48,10.745166 37.254834,0 24,0 C10.745166,0 0,10.745166 0,24 C0,20.6862915 2.6862915,18 6,18 C9.3137085,18 12,20.6862915 12,24 L12,24 C12,20.6862915 14.6862915,18 18,18 C21.3137085,18 24,20.6862915 24,24 C24,20.6862915 26.6862915,18 30,18 C33.3137085,18 36,20.6862915 36,24 C36,20.6862915 38.6862915,18 42,18 C45.3137085,18 48,20.6862915 48,24 Z"
+                                  id="Oval-1022"></path>
+                            <circle id="Oval-1026" cx="24.5" cy="46.5" r="2.5"></circle>
+                            <path d="M27,52 L31,48" id="Line"></path>
+                            <path d="M18,52 L22,48" id="Line-280"
+                                  transform="translate(20.000000, 50.000000) scale(-1, 1) translate(-20.000000, -50.000000) "></path>
+                            <path d="M2.00000001,27 L19.2173745,43.2692779" id="Path-2429"></path>
+                            <path d="M29.2883044,27 L46.5056789,43.2692779" id="Path-2429"
+                                  transform="translate(38.000000, 35.028512) scale(-1, 1) translate(-38.000000, -35.028512) "></path>
+                            <path d="M12.6258957,26.3556054 L22.2108584,40.8520585" id="Path-2431"></path>
+                            <path d="M26.5,26.3556054 L36.0849626,40.8520585" id="Path-2431"
+                                  transform="translate(31.292481, 33.603832) scale(-1, 1) translate(-31.292481, -33.603832) "></path>
+                            <path d="M23.9911083,22.9999973 L23.9911083,37.7549954" id="Path-2433"></path>
+                        </g>
+                    </g>
+                </g>
+            </svg>
+
+
+            <h4 class="block">Shop ready <i class="fa fa-angle-right m-l-md"></i></h4>
+            <p class="w-lg center">This is a very young collection of charts, with the goal of keeping these components
+                very customizable.</p>
+            <small class="block padder-v text-muted">Highly customizable NVD3, rickshaw</small>
+        </div>
+        <div class="col-sm-4 text-center">
+
+
+            <svg width="60px" height="60px" viewBox="0 0 60 60" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                <!-- Generator: Sketch 3.2.2 (9983) - http://www.bohemiancoding.com/sketch -->
+                <title>Prizemedalion</title>
+                <desc>Created with Sketch.</desc>
+                <defs></defs>
+                <g id="outlined" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                    <g id="Activities_sliced" sketch:type="MSLayerGroup"
+                       transform="translate(-720.000000, 0.000000)"></g>
+                    <g id="Activities" sketch:type="MSLayerGroup" transform="translate(-734.000000, -1.000000)"
+                       stroke="#535353" stroke-width="2" stroke-linejoin="round">
+                        <g id="Prizemedalion" transform="translate(741.000000, 9.000000)" sketch:type="MSShapeGroup">
+                            <circle id="Oval-810" stroke-linecap="round" cx="23" cy="33" r="14"></circle>
+                            <circle id="Oval-811" stroke-linecap="round" cx="23" cy="33" r="10"></circle>
+                            <path d="M20.2663117,29.378475 L23,27.5941876 L23,38" id="Path-1890"
+                                  stroke-linecap="round"></path>
+                            <path d="M27,19 L38,0" id="Line" stroke-linecap="square"></path>
+                            <path d="M8,19 L19,0" id="Line" stroke-linecap="square"
+                                  transform="translate(13.500000, 9.500000) scale(-1, 1) translate(-13.500000, -9.500000) "></path>
+                            <path d="M0,24 L11,5" id="Line" stroke-linecap="square"
+                                  transform="translate(5.500000, 14.500000) scale(-1, 1) translate(-5.500000, -14.500000) "></path>
+                            <path d="M35,24 L46,5" id="Line" stroke-linecap="square"></path>
+                        </g>
+                    </g>
+                </g>
+            </svg>
+
+
+            <h4 class="block">Shop ready <i class="fa fa-angle-right m-l-md"></i></h4>
+            <p class="w-lg center">This is a very young collection of charts, with the goal of keeping these components
+                very customizable.</p>
+            <small class="block padder-v text-muted">Highly customizable NVD3, rickshaw</small>
+        </div>
+        <div class="col-sm-4 text-center">
+
+
+            <svg width="60px" height="60px" viewBox="0 0 60 60" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                <!-- Generator: Sketch 3.2.2 (9983) - http://www.bohemiancoding.com/sketch -->
+                <title>Umbrella</title>
+                <desc>Created with Sketch.</desc>
+                <defs></defs>
+                <g id="outlined" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                    <g id="Activities_sliced" sketch:type="MSLayerGroup"
+                       transform="translate(-240.000000, 0.000000)"></g>
+                    <g id="Activities" sketch:type="MSLayerGroup" transform="translate(-254.000000, -1.000000)"
+                       stroke="#535353" stroke-width="2" stroke-linejoin="round">
+                        <g id="Umbrella" transform="translate(260.000000, 5.000000)" sketch:type="MSShapeGroup">
+                            <path d="M12.9170416,31 C12.4409635,33.8377133 9.97299629,36 7,36 C4.02700371,36 1.55903653,33.8377133 1.08295844,31 L1,31 L1,24 L49,24 L49,31 L48.9170416,31 C48.4409635,33.8377133 45.9729963,36 43,36 C40.0270037,36 37.5590365,33.8377133 37.0829584,31 L36.9170416,31 C36.4409635,33.8377133 33.9729963,36 31,36 C28.0270037,36 25.5590365,33.8377133 25.0829584,31 L24.9170416,31 C24.4409635,33.8377133 21.9729963,36 19,36 C16.0270037,36 13.5590365,33.8377133 13.0829584,31 L12.9170416,31 Z"
+                                  id="Oval-800" stroke-linecap="round"></path>
+                            <polygon id="Triangle-27" stroke-linecap="round" points="25 4 50 24 0 24 "></polygon>
+                            <path d="M25,4 L25,0" id="Line" stroke-linecap="square"></path>
+                            <path d="M23,39 L23,54" id="Line" stroke-linecap="square"></path>
+                            <path d="M27,39 L27,54" id="Line" stroke-linecap="square"></path>
+                        </g>
+                    </g>
+                </g>
+            </svg>
+
+            <h4 class="block">Shop ready <i class="fa fa-angle-right m-l-md"></i></h4>
+            <p class="w-lg center">This is a very young collection of charts, with the goal of keeping these components
+                very customizable.</p>
+            <small class="block padder-v text-muted">Highly customizable NVD3, rickshaw</small>
+        </div>
+    </div>
+
+
+</div>
+
+
+<!-- footer -->
+<footer id="footer" class="b-t m-t-md">
+    <div class="container">
+        <div class="row padder-v m-t">
+            <div class="col-xs-8">
+                <ul class="list-inline">
+                    <li><a href="#">Карта сайта</a></li>
+                    <li><a href="#">Инвестору</a></li>
+                    <li><a href="#">Потребителям</a></li>
+                    <li><a href="#">Контакты</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-4 text-right">
+                Angulr &copy; 2015
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- / footer -->
+
+
+</body>
+
+
+<script src="/dist/js/orchid.js"></script>
+</html>
