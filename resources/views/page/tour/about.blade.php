1934bd332ecb90baee469d35d0c3f414a52335a1
tabuna <bliz48rus@gmail.com>
2016-11-07 11:30:50 +0300

    О липецкой области

--- resources/views/page/tour/about.blade.php
+++ resources/views/page/tour/about.blade.php
@@ -7,7 +7,7 @@
 
     <div class="about" id="about-container">
         <section class="bg-dark" style="
-background: url(http://www.votpusk.ru/story/edit/foto/large/47022.jpg);
+background: url(/img/tour/about/hero1.jpg);
     background-size: cover;
 ">
             <div class="bg-black-opacity v-h-center h-full">
@@ -23,7 +23,22 @@ background: url(http://www.votpusk.ru/story/edit/foto/large/47022.jpg);
 
         </section>
         <section class="bg-dark" style="
-background: url(https://upload.wikimedia.org/wikipedia/commons/c/cd/%D0%9B%D0%B8%D0%BF%D0%B5%D1%86%D0%BA._%D0%94%D0%BE%D0%BC_%D0%A1%D0%BE%D0%B2%D0%B5%D1%82%D0%BE%D0%B2.jpg);
+background: url(/img/tour/about/hero2.jpg);   background-size: cover;
+">
+            <div class="bg-black-opacity v-h-center h-full">
+                <div class="container">
+                    <p>
+                        Люди селились здесь с древнейших времен. Первое по датировке поселение (Гагаринская стоянка)
+                        относится к палеолиту и датируется возрастом в 22 тыс. лет. На территории современного Липецка
+                        (стоянка «Липецкое озеро») проживали люди эпохи неолита (3 - 5 тысячелетие до новой эры). На
+                        территории края кочевали скифы и половцы, строили укрепленные городища финно-угры.
+                    </p>
+                </div>
+            </div>
+
+        </section>
+        <section class="bg-dark" style="
+background: url(/img/tour/about/hero3.jpg);
     background-size: cover;
 ">
             <div class="bg-black-opacity v-h-center h-full">
@@ -39,7 +54,7 @@ background: url(https://upload.wikimedia.org/wikipedia/commons/c/cd/%D0%9B%D0%B8
 
         </section>
         <section class="bg-dark" style="
-background: url(http://www.lokg.ru/images/stories/lections/dom_gubina.jpg);
+background: url(/img/tour/about/hero4.jpg);
     background-size: cover;
 ">
             <div class="bg-black-opacity v-h-center h-full">
@@ -55,8 +70,40 @@ background: url(http://www.lokg.ru/images/stories/lections/dom_gubina.jpg);
 
         </section>
         <section class="bg-dark" style="
-background: url(http://cs629420.vk.me/v629420879/126a8/t0GGZuPA-6Q.jpg);
-    background-size: cover;
+background: url(/img/tour/about/hero5.jpg); background-size: cover;
+">
+            <div class="bg-black-opacity v-h-center h-full">
+                <div class="container">
+                    <p>
+                        Люди селились здесь с древнейших времен. Первое по датировке поселение (Гагаринская стоянка)
+                        относится к палеолиту и датируется возрастом в 22 тыс. лет. На территории современного Липецка
+                        (стоянка «Липецкое озеро») проживали люди эпохи неолита (3 - 5 тысячелетие до новой эры). На
+                        территории края кочевали скифы и половцы, строили укрепленные городища финно-угры.
+                    </p>
+                </div>
+            </div>
+
+        </section>
+
+
+        <section class="bg-dark" style="
+background: url(/img/tour/about/hero6.jpg); background-size: cover;
+">
+            <div class="bg-black-opacity v-h-center h-full">
+                <div class="container">
+                    <p>
+                        Люди селились здесь с древнейших времен. Первое по датировке поселение (Гагаринская стоянка)
+                        относится к палеолиту и датируется возрастом в 22 тыс. лет. На территории современного Липецка
+                        (стоянка «Липецкое озеро») проживали люди эпохи неолита (3 - 5 тысячелетие до новой эры). На
+                        территории края кочевали скифы и половцы, строили укрепленные городища финно-угры.
+                    </p>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="bg-dark" style="
+background: url(/img/tour/about/hero7.jpg); background-size: cover;
 ">
             <div class="bg-black-opacity v-h-center h-full">
                 <div class="container">
@@ -71,8 +118,23 @@ background: url(http://cs629420.vk.me/v629420879/126a8/t0GGZuPA-6Q.jpg);
 
         </section>
         <section class="bg-dark" style="
-background: url(http://3.bp.blogspot.com/-hOGJDXRNGv4/VQM_txkzv5I/AAAAAAAADzU/x4NQcWF5x6c/s1600/Lovestory,%2B%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%2B%D0%BB%D0%B8%D0%BF%D0%B5%D1%86%D0%BA,%2B%D1%81%D0%B2%D0%B0%D0%B4%D0%B5%D0%B1%D0%BD%D1%8B%D0%B9%2B%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84,%2B%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%2B%D0%BD%D0%B0%2B%D1%81%D0%B2%D0%B0%D0%B4%D1%8C%D0%B1%D1%83%2B%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2B%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D0%B5%D0%B6%2B%D0%9B%D0%B8%D0%BF%D0%B5%D1%86%D0%BA,%2B%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82%D0%BD%D0%B0%D1%8F%2B%D1%84%D0%BE%D1%82%D0%BE%D1%81%D0%B5%D1%81%D1%81%D0%B8%D1%8F,%2B%D1%84%D0%BE%D1%82%D0%BE%D1%81%D1%8A%D0%B5%D0%BC%D0%BA%D0%B0,%2B%D0%BB%D1%8E%D0%B1%D0%BE%D0%B2%D1%8C,%2B%D1%80%D0%BE%D0%BC%D0%B0%D0%BD%D1%82%D0%B8%D0%BA%D0%B0,%2B%D1%84%D0%BE%D1%82%D0%BE%D1%81%D0%B5%D1%82_14.jpg);
-    background-size: cover;
+background: url(/img/tour/about/hero8.jpg); background-size: cover;
+">
+            <div class="bg-black-opacity v-h-center h-full">
+                <div class="container">
+                    <p>
+                        Люди селились здесь с древнейших времен. Первое по датировке поселение (Гагаринская стоянка)
+                        относится к палеолиту и датируется возрастом в 22 тыс. лет. На территории современного Липецка
+                        (стоянка «Липецкое озеро») проживали люди эпохи неолита (3 - 5 тысячелетие до новой эры). На
+                        территории края кочевали скифы и половцы, строили укрепленные городища финно-угры.
+                    </p>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="bg-dark" style="
+background: url(/img/tour/about/hero9.jpg); background-size: cover;
 ">
             <div class="bg-black-opacity v-h-center h-full">
                 <div class="container">
